9b71f0f5c5a186c2c0ceaaf9ee28278d78695cf1
Khang Nguyen <khang.eqkai@gmail.com>
2023-08-05 18:58:47 +1000

    changed code in ass.py

--- ass.py
+++ ass.py
@@ -189,19 +189,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# 5. Set this as an API endpoint via FastAPI
-# app = FastAPI()
-
-
-# class Query(BaseModel):
-#     query: str
-
-
-# @app.post("/")
-# def researchAgent(query: Query):
-#     query = query.query
-#     content = agent({"input": query})
-#     actual_content = content['output']
-#     return actual_content
